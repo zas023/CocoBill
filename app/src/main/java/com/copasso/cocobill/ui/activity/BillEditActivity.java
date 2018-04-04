b931cc907a0a321f4741b19ee190e30c62b725de
zas023 <375027533@qq.com>
2018-04-04 17:28:28 +0800

    修复修改账单bug

--- app/src/main/java/com/copasso/cocobill/ui/activity/BillEditActivity.java
+++ app/src/main/java/com/copasso/cocobill/ui/activity/BillEditActivity.java
@@ -145,7 +145,7 @@ public class BillEditActivity extends BillAddActivity implements BillView {
         }
 
         ProgressUtils.show(mContext, "正在提交...");
-        presenter.add(new BBill(bundle.getLong("id"),bundle.getString("rid"),
+        presenter.update(new BBill(bundle.getLong("id"),bundle.getString("rid"),
                 Float.valueOf(num + dotNum),remarkInput,currentUser.getObjectId(),
                 noteBean.getPayinfo().get(selectedPayinfoIndex).getPayName(),
                 noteBean.getPayinfo().get(selectedPayinfoIndex).getPayImg(),
